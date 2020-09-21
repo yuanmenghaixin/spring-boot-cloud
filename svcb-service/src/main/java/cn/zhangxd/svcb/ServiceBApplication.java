caeeab71cdbf356d84ef3d0e81daace62466dc7d
peng.lv <peng.lv@geely.com>
2020-09-21 18:41:03 +0800

    高可用

--- svcb-service/src/main/java/cn/zhangxd/svcb/ServiceBApplication.java
+++ svcb-service/src/main/java/cn/zhangxd/svcb/ServiceBApplication.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+//开启断路器工能
 @EnableCircuitBreaker
 @EnableOAuth2Client
 public class ServiceBApplication {
